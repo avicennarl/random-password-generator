5d45f9f0290d373ca448eae3e8bc769c6fbedf35
avicennarl <92698716+avicennarl@users.noreply.github.com>
2023-05-21 03:23:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ import random
 import string
 
 print ("Welcome to Password Generator!")
+length = int(input('\nEnter the length of password: '))
+
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+
+all = lower + upper + num
+
+temp = random.sample(all,length)
+password = "".join(temp)
+
+print(password)
